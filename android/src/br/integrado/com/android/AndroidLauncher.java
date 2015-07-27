2d4a7e5a3fbc3c197f1a1dace4adad681d77cf57
Gustavo Mantuan Cerqueira Alves <gustavo_mantuan10@hotmail.com>
2015-07-27 20:19:00 -0300

    Primeir Commit do Joguinho

---
+++ android/src/br/integrado/com/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.integrado.com.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.integrado.com.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
