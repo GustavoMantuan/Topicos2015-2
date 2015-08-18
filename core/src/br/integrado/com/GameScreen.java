fa364c9330ab4317e52614241ce0c792afd45ca3
Gustavo Mantuan Cerqueira Alves <gustavo_mantuan10@hotmail.com>
2015-08-17 21:04:39 -0300

    Tirinho e Meteoro

--- core/src/br/integrado/com/GameScreen.java
+++ core/src/br/integrado/com/GameScreen.java
@@ -9,10 +9,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
 /**
@@ -33,8 +35,14 @@ public class GameScreen extends BaseScreen {
     private boolean goingRight;
     private boolean goingUp;
     private boolean goingDown;
-    float velocidade = 10; // velocidade de movimento do jogador.
-
+    float velocidade = 300; // velocidade de movimento do jogador.
+    private boolean shoting;
+    private Array<Image> shotings = new Array<Image>();
+    private Texture texturaShot;
+    private Texture meteor_1;
+    private Texture meteor_2;
+    private Array<Image> meteoros_1 = new Array<Image>();
+    private Array<Image> meteoros_2 = new Array<Image>();
 
     /**
      * Construtor padrão da tela de jogo
@@ -52,11 +60,21 @@ public class GameScreen extends BaseScreen {
         camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         batch = new SpriteBatch();
         palco = new Stage(new FillViewport(camera.viewportWidth,camera.viewportHeight, camera));
+        initTexturas();
         initFonte();
         initInformacoes();
         initPlayer();
     }
 
+    private void initTexturas() {
+        texturaShot = new Texture("sprites/shot.png");
+        meteor_1 = new Texture("sprites/enemie-1.png");
+        meteor_2 = new Texture("sprites/enemie-2.png");
+    }
+
+    /**
+     * Instancia os objetos do jogador e adiciona um palco.
+     */
     private void initPlayer() {
         playerTextureMid = new Texture("sprites/player.png");
         playerTextureLeft = new Texture("sprites/player-left.png");
@@ -68,20 +86,23 @@ public class GameScreen extends BaseScreen {
         palco.addActor(player);
     }
 
+    /**
+     * Instancia as informações descritas na tela.
+     */
     private void initInformacoes() {
         Label.LabelStyle lbEstilo = new Label.LabelStyle();
         lbEstilo.font = fonte;
         lbEstilo.fontColor = Color.WHITE;
-
         lbPontuacao = new Label("0 Pontos", lbEstilo);
-
         palco.addActor(lbPontuacao);
     }
 
+    /**
+     * Instancia a fonte
+     */
     private void initFonte() {
         fonte = new BitmapFont();
     }
-
 
     /**
      * Chamado a todo quadro de atualização do jogo (fps)
@@ -94,9 +115,61 @@ public class GameScreen extends BaseScreen {
         lbPontuacao.setPosition(10, camera.viewportHeight - 20);
         capturaTeclas();
         atualizarPlayer(delta);
+        // Atualiza a situação do palco
         palco.act(delta);
+        // Desenha o palco na tela
         palco.draw();
+        atualizarShot(delta);
+        atualizaMeteoros(delta);
+    }
+    private final float MAX_INTERVALO_TIROS = 0.4f;
+    private float intervaloTiros = 0;
 
+    private void atualizarShot(float delta) {
+        intervaloTiros = intervaloTiros + delta;
+        if (shoting){
+            if (intervaloTiros >= MAX_INTERVALO_TIROS){
+                Image shot = new Image(texturaShot);
+                float x = player.getX()+player.getWidth()/2 - shot.getWidth()/2;
+                float y = player.getY()+player.getHeight();
+                shot.setPosition(x,y);
+                shotings.add(shot);
+                palco.addActor(shot);
+                intervaloTiros = 0;
+            }
+        }
+        float velocidade = 8;
+        for (Image tiro: shotings){
+            //movimenta os tiros
+            float x = tiro.getX();
+            float y = tiro.getY() + velocidade + delta;
+            tiro.setPosition(x,y);
+            //remove os tiros que sairam da tela
+            if(tiro.getY() > camera.viewportHeight){
+                shotings.removeValue(tiro,true); // remove da lista
+                tiro.remove(); // rmeove do palco
+            }
+
+        }
+    }
+
+    private void atualizaMeteoros(float delta){
+        int tipo = MathUtils.random(1,3);
+        if (tipo == 1 ){
+            Image meteoro = new Image(meteor_1);
+            float x = MathUtils.random(0,camera.viewportWidth - meteoro.getWidth());
+            float y = MathUtils.random(camera.viewportHeight,camera.viewportHeight*2);
+            meteoro.setPosition(x,y);
+            meteoros_1.add(meteoro);
+            palco.addActor(meteoro);
+        }else {
+
+            //2
+        }
+        float velocidade = 100;
+        for (Image m : meteoros_1){
+
+        }
     }
 
     /**
@@ -105,9 +178,9 @@ public class GameScreen extends BaseScreen {
      */
     private void atualizarPlayer(float delta) {
         if(goingRight && player.getX() < camera.viewportWidth - player.getImageWidth()){
+            // Verifica se o jogador está dentro da tela.
             float x = player.getX() + velocidade * delta;
             float y = player.getY();
-
             player.setPosition(x,y);
         }
         if (goingLeft && player.getX() > 0) {
@@ -132,10 +205,7 @@ public class GameScreen extends BaseScreen {
             player.setDrawable(new SpriteDrawable(new Sprite(playerTextureLeft)));
         }else{
             player.setDrawable(new SpriteDrawable(new Sprite(playerTextureMid)));
-
         }
-
-
     }
 
     /**
@@ -146,14 +216,9 @@ public class GameScreen extends BaseScreen {
         goingLeft = false;
         goingUp = false;
         goingDown = false;
-        int tecla;
+        shoting = false;
 
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            tecla = Input.Keys.LEFT;
-            if (tecla == Input.Keys.LEFT && velocidade <= 300){velocidade += 10;}
-            else{
-                velocidade = 10;
-            };
             goingLeft = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
@@ -164,6 +229,9 @@ public class GameScreen extends BaseScreen {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
             goingDown = true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            shoting = true;
         }
     }
 
@@ -205,5 +273,8 @@ public class GameScreen extends BaseScreen {
         playerTextureMid.dispose();
         playerTextureLeft.dispose();
         playerTextureRight.dispose();
+        texturaShot.dispose();
+        meteor_1.dispose();
+        meteor_2.dispose();
     }
 }
